1b6829753b0d7a751245838690968274109e34d5
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-10-13 19:16:47 +0530

    cors applied

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express=require("express")
+const express = require("express")
 const { default: mongoose } = require("mongoose")
 const app = express()
 require("dotenv").config()
+const cors = require('cors');
 
 const userRouter = require("./routes/userRoutes")
 
 app.use(express.json())
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+};
 
-const connection = async()=>{
+app.use(cors(corsOptions));
+
+
+const connection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL)
     } catch (error) {
@@ -16,11 +23,11 @@ const connection = async()=>{
     }
 }
 
-app.use("/users",userRouter)
+app.use("/users", userRouter)
 
 const PORT = process.env.PORT
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connection()
     console.log("port is running on 7000");
 })
